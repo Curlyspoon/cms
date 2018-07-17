c9df76a28b5c9fad55890ac49e993f3ada036e9f
Tom Witkowski <dev.gummibeer@gmail.com>
2018-07-17 11:51:27 +0200

    add nested options resolver https://github.com/Curlyspoon/core/issues/2

--- src/Libs/Element.php
+++ src/Libs/Element.php
@@ -4,8 +4,7 @@ namespace Curlyspoon\Cms\Libs;
 
 use Illuminate\Container\Container;
 use Curlyspoon\Core\Elements\BladeElement;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Curlyspoon\Cms\Contracts\NormalizerManager as NormalizerManagerContract;
+use Symfony\Component\OptionsResolver\OptionsResolver as SymfonyOptionsResolver;
 
 abstract class Element extends BladeElement
 {
@@ -17,29 +16,28 @@ abstract class Element extends BladeElement
      */
     protected $normalizers = [];
 
+    /**
+     * @var array
+     *
+     * @see OptionsResolver::setNested()
+     */
+    protected $nested = [];
+
     protected function getPath(): string
     {
         return Container::getInstance()->make('config')->get('curlyspoon.element_view_prefix');
     }
 
-    protected function optionsResolver(): OptionsResolver
+    protected function optionsResolver(): SymfonyOptionsResolver
     {
-        $resolver = new OptionsResolver();
-        $resolver->setDefaults($this->defaults);
-
-        $resolver->setRequired($this->required);
-
-        foreach ($this->types as $option => $types) {
-            $resolver->setAllowedTypes($option, $types);
-        }
-
-        foreach ($this->values as $option => $values) {
-            $resolver->setAllowedValues($option, $values);
-        }
-
-        foreach ($this->normalizers as $option => $normalizer) {
-            $resolver->setNormalizer($option, Container::getInstance()->make(NormalizerManagerContract::class)->normalizer($normalizer));
-        }
+        $resolver = OptionsResolver::make([
+            'defaults' => $this->defaults,
+            'required' => $this->required,
+            'types' => $this->types,
+            'values' => $this->values,
+            'normalizers' => $this->normalizers,
+            'nested' => $this->nested,
+        ]);
 
         if (method_exists($this, 'configureOptions')) {
             $this->configureOptions($resolver);
