a7d86f815a799373145982e29e6a02a9a748e99a
Tom Witkowski <dev.gummibeer@gmail.com>
2018-07-17 09:51:49 +0000

    Apply fixes from StyleCI

--- src/Libs/OptionsResolver.php
+++ src/Libs/OptionsResolver.php
@@ -3,8 +3,8 @@
 namespace Curlyspoon\Cms\Libs;
 
 use Illuminate\Container\Container;
-use Symfony\Component\OptionsResolver\OptionsResolver as SymfonyOptionsResolver;
 use Curlyspoon\Cms\Contracts\NormalizerManager as NormalizerManagerContract;
+use Symfony\Component\OptionsResolver\OptionsResolver as SymfonyOptionsResolver;
 
 class OptionsResolver extends SymfonyOptionsResolver
 {
@@ -14,33 +14,33 @@ class OptionsResolver extends SymfonyOptionsResolver
     {
         $resolver = new static();
 
-        if(!empty($config['defaults'])) {
+        if (! empty($config['defaults'])) {
             $resolver->setDefaults($config['defaults']);
         }
 
-        if(!empty($config['required'])) {
+        if (! empty($config['required'])) {
             $resolver->setRequired($config['required']);
         }
 
-        if(!empty($config['types'])) {
+        if (! empty($config['types'])) {
             foreach ($config['types'] as $option => $types) {
                 $resolver->setAllowedTypes($option, $types);
             }
         }
 
-        if(!empty($config['values'])) {
+        if (! empty($config['values'])) {
             foreach ($config['values'] as $option => $values) {
                 $resolver->setAllowedValues($option, $values);
             }
         }
 
-        if(!empty($config['normalizers'])) {
+        if (! empty($config['normalizers'])) {
             foreach ($config['normalizers'] as $option => $normalizer) {
                 $resolver->setNormalizer($option, Container::getInstance()->make(NormalizerManagerContract::class)->normalizer($normalizer));
             }
         }
 
-        if(!empty($config['nested'])) {
+        if (! empty($config['nested'])) {
             foreach ($config['nested'] as $option => $nestedConfig) {
                 $resolver->setNested($option, static::make($nestedConfig));
             }
@@ -58,10 +58,10 @@ class OptionsResolver extends SymfonyOptionsResolver
 
     public function isNested(): bool
     {
-        return !empty($this->nested);
+        return ! empty($this->nested);
     }
 
-    public function resolve(array $options = array())
+    public function resolve(array $options = [])
     {
         $resolved = parent::resolve($options);
 
@@ -71,13 +71,13 @@ class OptionsResolver extends SymfonyOptionsResolver
                 $key = substr($key, 0, -2);
             }
             $dataToResolve = array_get($resolved, $key);
-            if (!$isLoop) {
+            if (! $isLoop) {
                 array_set($resolved, $key, $resolver->resolve($dataToResolve));
                 continue;
             }
 
             foreach ($dataToResolve as $subKey => $data) {
-                array_set($resolved, $key . '.' . $subKey, $resolver->resolve($data));
+                array_set($resolved, $key.'.'.$subKey, $resolver->resolve($data));
             }
         }
 
